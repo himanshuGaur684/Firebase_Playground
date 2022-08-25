2baf4d56e3e66dcf397dced888c23e297e274bac
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2022-08-25 16:10:11 +0530

    firebase Storage

--- app/src/main/java/com/gaur/firebasetutorials/MainActivity.kt
+++ app/src/main/java/com/gaur/firebasetutorials/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.gaur.firebasetutorials
 
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.gaur.firebasetutorials.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,73 +21,80 @@ class MainActivity : AppCompatActivity() {
         get() = _binding!!
 
 
-    private val auth = FirebaseAuth.getInstance()
+    private val storage = FirebaseStorage.getInstance()
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            it?.let {
+                if (it) {
+                    this.makeToast("permission granted")
+                }
+            }
+        }
+
+    private val fileAccess =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            it.data?.data?.let {
+                val inputStream = this.contentResolver.openInputStream(it)
+                inputStream?.readBytes()?.let {
+                    uploadFile(it)
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.btnSignUp.setOnClickListener {
-            val email = binding.edEmail.text.toString()
-            val password = binding.edPassword.text.toString()
-
-            when {
-                email.isEmpty() -> {
-                    binding.edEmail.requestFocus()
-                }
-                password.isEmpty() || password.length <= 6 -> {
-                    binding.edPassword.requestFocus()
-                }
-                else -> {
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                        this.makeToast("complete")
-                    }
-                        .addOnCanceledListener {
-                            this.makeToast("event cancel")
-                        }.addOnSuccessListener {
-                            this.makeToast(it.user?.email.toString())
-                        }
-                }
-            }
-
-        }
-
-        binding.btnUserLogout.setOnClickListener {
-            if (auth.currentUser != null) {
-                auth.signOut()
-            } else {
-                this.makeToast("user already logout")
-            }
-        }
-
-        binding.btnSignIn.setOnClickListener {
-            val email = binding.edEmail.text.toString()
-            val password = binding.edPassword.text.toString()
-
-            when {
-                email.isEmpty() -> {
-                    binding.edEmail.requestFocus()
-                }
-                password.isEmpty() || password.length <= 6 -> {
-                    binding.edPassword.requestFocus()
-                }
-                else -> {
-                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                        this.makeToast("complete")
-                    }
-                        .addOnCanceledListener {
-                            this.makeToast("event cancel")
-                        }.addOnSuccessListener {
-                            this.makeToast(it.user?.email.toString())
-                        }
-                }
+        binding.btnUploadFilesOnFirebase.setOnClickListener {
+            sdkIntOverO(this) {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "*/*"
+                fileAccess.launch(intent)
             }
         }
 
 
     }
+
+    fun uploadFile(byteArray: ByteArray) {
+        val storageRef = storage.reference
+        val storageRef2 = storageRef.child("images/${Date().time}")
+        storageRef2.putBytes(byteArray)
+            .addOnSuccessListener {
+                this.makeToast("success")
+                // taking the public url
+                storageRef2.downloadUrl.addOnSuccessListener {
+                    Log.d("TAG", "uploadFile: $it")
+                }
+
+            }
+            .addOnFailureListener {
+                this.makeToast("failure")
+            }
+            .addOnCompleteListener {
+                this.makeToast("complete")
+            }
+
+    }
+
+    fun sdkIntOverO(context: Context, call: () -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                call.invoke()
+            } else {
+                requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+
 }
 
 fun Context.makeToast(message: String) {
