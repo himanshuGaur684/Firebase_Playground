e08113c8e074d7a1499a65c21db61815c0b526bf
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2022-09-03 16:10:33 +0530

    firebase Firestore CRUD Operation.

--- app/src/main/java/com/gaur/firebasetutorials/MainActivity.kt
+++ app/src/main/java/com/gaur/firebasetutorials/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.gaur.firebasetutorials
 
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.gaur.firebasetutorials.databinding.ActivityMainBinding
-import com.google.firebase.storage.FirebaseStorage
-import java.util.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,80 +17,85 @@ class MainActivity : AppCompatActivity() {
         get() = _binding!!
 
 
-    private val storage = FirebaseStorage.getInstance()
+    private val db: FirebaseFirestore by lazy { Firebase.firestore }
 
-    private val requestPermission =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            it?.let {
-                if (it) {
-                    this.makeToast("permission granted")
-                }
-            }
-        }
+    private val TITLE = "title"
+    private val DESCRIPTION = "description"
+    private val COLLECTION = "notes"
+    private val DOCUMENT = "inner_notes"
 
-    private val fileAccess =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            it.data?.data?.let {
-                val inputStream = this.contentResolver.openInputStream(it)
-                inputStream?.readBytes()?.let {
-                    uploadFile(it)
-                }
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnUploadFilesOnFirebase.setOnClickListener {
-            sdkIntOverO(this) {
-                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-                intent.addCategory(Intent.CATEGORY_OPENABLE)
-                intent.type = "*/*"
-                fileAccess.launch(intent)
-            }
+        binding.btnSave.setOnClickListener {
+            save()
         }
-
+        binding.btnRead.setOnClickListener {
+            read()
+        }
+        binding.btnUpdate.setOnClickListener {
+            update()
+        }
+        binding.btnDelete.setOnClickListener {
+            delete()
+        }
 
     }
 
-    fun uploadFile(byteArray: ByteArray) {
-        val storageRef = storage.reference
-        val storageRef2 = storageRef.child("images/${Date().time}")
-        storageRef2.putBytes(byteArray)
-            .addOnSuccessListener {
-                this.makeToast("success")
-                // taking the public url
-                storageRef2.downloadUrl.addOnSuccessListener {
-                    Log.d("TAG", "uploadFile: $it")
-                }
 
+    private fun delete() {
+        db.collection(COLLECTION).document(DOCUMENT).delete().addOnSuccessListener {
+            this.makeToast("deleted")
+        }.addOnFailureListener {
+            this.makeToast("error occurred $it")
+        }
+    }
+
+    private fun update() {
+        db.collection(COLLECTION).document(DOCUMENT)
+//             also update with update function
+//            .update(mutableMapOf<String,Any>(TITLE to "titles"))
+            .set(mutableMapOf(TITLE to "titles"), SetOptions.merge())
+            .addOnSuccessListener {
+                this.makeToast("updated")
+            }.addOnFailureListener {
+                this.makeToast("error occurred $it")
+            }
+    }
+
+    private fun read() {
+        db.collection(COLLECTION).document(DOCUMENT).get().addOnSuccessListener {
+            val title = it.get(TITLE)
+            val desc = it.get(DESCRIPTION)
+            binding.tvRead.text = "Title: $title\nDescription : $desc"
+        }.addOnFailureListener {
+            this.makeToast("Error occurred $it")
+        }
+    }
+
+    private fun save() {
+        val title = binding.edTitle.text.toString()
+        val desc = binding.edDescription.text.toString()
+        val map = mutableMapOf<String, String>()
+        map.put(TITLE, title)
+        map.put(DESCRIPTION, desc)
+
+//        create unique id for your document
+//        db.collection(COLLECTION).document().set(map)
+//        or
+//        db.collection(COLLECTION).add(map)
+
+        db.collection(COLLECTION).document(DOCUMENT).set(map)
+            .addOnSuccessListener {
+                this.makeToast("Successfully saved.")
             }
             .addOnFailureListener {
-                this.makeToast("failure")
+                this.makeToast("Error Occurred $it")
             }
-            .addOnCompleteListener {
-                this.makeToast("complete")
-            }
-
     }
-
-    fun sdkIntOverO(context: Context, call: () -> Unit) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    android.Manifest.permission.READ_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                call.invoke()
-            } else {
-                requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
-        }
-    }
-
-
 }
 
 fun Context.makeToast(message: String) {
